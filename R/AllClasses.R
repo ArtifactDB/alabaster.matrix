76bba417fb3c7cec0a6c16826ba0634366ff132a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-10 16:45:14 -0700

    Began working on the MultiSampleMatrix.

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,3 +1,11 @@
 #' @export
 #' @import methods
 setClass("WrapperArraySeed", contains="VIRTUAL", slots=c(seed="ANY"))
+
+#' @export
+#' @importClassesFrom DelayedArray DelayedAbind
+setClass("MultiSampleMatrixSeed", contains="DelayedAbind", slots=c(samples = "character"))
+
+#' @export
+#' @importClassesFrom DelayedArray DelayedMatrix
+setClass("MultiSampleMatrix", contains="DelayedMatrix", slots=c(seed = "MultiSampleMatrixSeed"))
