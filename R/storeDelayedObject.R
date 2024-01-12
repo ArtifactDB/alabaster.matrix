4f970d359821238c3818725793d8ed9e8970a8d5
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-01-11 16:45:41 -0800

    Begin adding chihaya support.

---
+++ R/storeDelayedObject.R
@@ -0,0 +1,209 @@
+#' Store operations in a DelayedArray
+#'
+#' Store the delayed operations of a \linkS4class{DelayedArray} in a HDF5 file.
+#'
+#' @param x Any of the delayed operation classes from \pkg{DelayedArray}.
+#' @param file String containing the path to a HDF5 file.
+#' @param name String containing the name of the group to save into.
+#' @param ... Arguments to be passed to specific methods.
+#' @return The contents of \code{x} are saved to \code{file}, and \code{NULL} is invisibly returned.
+#' 
+#' @author Aaron Lun
+#' @examples
+#' library(DelayedArray)
+#' X <- DelayedArray(matrix(runif(100), ncol=20))
+#' Y <- cbind(X, DelayedArray::ConstantArray(value=50, c(5, 10)))
+#'
+#' library(rhdf5)
+#' temp <- tempfile()
+#' dir.create(temp)
+#'
+#' fpath <- file.path(temp, "foo.h5")
+#' fhandle <- H5Fcreate(fpath)
+#' storeDelayedObject(Y@seed, fhandle, "YAY")
+#' rhdf5::h5ls(fpath)
+#' H5Fclose(fhandle)
+#'
+#' fhandle <- H5Fopen(fpath, "H5F_ACC_RDONLY")
+#' ghandle <- H5Gopen(fhandle, "YAY")
+#' reloadDelayedObject(ghandle)
+#' H5Gclose(ghandle)
+#' H5Fclose(fhandle)
+#'
+#' @aliases
+#' storeDelayedObject
+#' reloadDelayedObject
+#' storeDelayedObject,ConstantArraySeed-method
+#' storeDelayedObject,DelayedAbind-method
+#' storeDelayedObject,ANY-method
+#'
+#' @name storeDelayedObject
+NULL
+
+r2value_type_mapping <- c(logical="BOOLEAN", integer="INTEGER", double="FLOAT", character="STRING")
+to_value_type <- function(type) {
+    if (!(type %in% names(r2value_type_mapping))) {
+        stop("cannot map type '", type, "' to a value type")
+    }
+    r2value_type_mapping[[type]]
+}
+
+value2alabaster_type_mapping <- c(BOOLEAN="boolean", INTEGER="integer", FLOAT="number", STRING="string")
+to_alabaster_type <- function(vtype) {
+    if (!(vtype %in% names(value2alabaster_type_mapping))) {
+        stop("cannot map type '", type, "' to an alabaster type")
+    }
+    value2alabaster_type_mapping[[vtype]]
+}
+
+#' @import alabaster.base rhdf5
+load_vector_for_chihaya <- function(handle, name, version) {
+    dhandle <- H5Dopen(handle, name)
+    on.exit(H5Dclose(dhandle), add=TRUE, after=FALSE)
+    contents <- H5Dread(dhandle, drop=TRUE) 
+    if (is.raw(contents)) {
+        storage.mode(contents) <- "integer"
+    }
+
+    type <- h5_read_attribute(dhandle, "type")
+    type <- to_alabaster_type(type)
+    missing.placeholder <- h5_read_attribute(dhandle, "missing_placeholder", check=TRUE, default=NULL)
+    h5_cast(contents, expected.type=type, missing.placeholder=missing.placeholder)
+}
+
+#' @import alabaster.base rhdf5
+save_vector_for_chihaya <- function(handle, name, x, version, scalar) {
+    info <- transformVectorForHdf5(x)
+    dhandle <- h5_write_vector(handle, name, info$transformed, scalar=scalar, emit=TRUE)
+    on.exit(H5Dclose(dhandle), add=TRUE, after=FALSE)
+
+    if (!is.null(info$placeholder)) {
+        h5_write_attribute(dhandle, "missing_placeholder", info$placeholder, scalar=TRUE)
+    }
+    h5_write_attribute(dhandle, "type", to_value_type(type(x)), scalar=TRUE)
+
+    invisible(NULL)
+}
+
+#######################################################
+#######################################################
+
+chihaya_array_registry <- list()
+chihaya_operation_registry <- list()
+
+#' @export
+reloadDelayedObject <- function(handle, version=package_version("1.1"), ...) {
+    objtype <- h5_read_attribute(handle, "delayed_type")
+
+    if (objtype == "array") {
+        arrtype <- h5_read_attribute(handle, "delayed_array")
+        FUN <- chihaya_array_registry[[arrtype]]
+    } else {
+        optype <- h5_read_attribute(handle, "delayed_operation")
+        FUN <- chihaya_operation_registry[[optype]]
+    }
+
+    FUN(handle, version=version, ...)
+}
+
+#######################################################
+#######################################################
+
+#' @export
+#' @import rhdf5
+setMethod("storeDelayedObject", "ConstantArraySeed", function(x, handle, name, version=package_version("1.1"), ...) {
+    ghandle <- H5Gcreate(handle, name)
+    on.exit(H5Gclose(ghandle), add=TRUE, after=FALSE)
+
+    h5_write_attribute(ghandle, "delayed_type", "array", scalar=TRUE)
+    h5_write_attribute(ghandle, "delayed_array", "constant array", scalar=TRUE)
+    h5_write_vector(ghandle, "dimensions", dim(x), compress=0, type="H5T_NATIVE_UINT32")
+    save_vector_for_chihaya(ghandle, "value", x@value, version=version, scalar=TRUE)
+})
+
+#' @import rhdf5 DelayedArray
+chihaya_array_registry[["constant array"]] <- function(handle, version, ...) {
+    dim <- h5_read_vector(handle, "dimensions")
+    val <- load_vector_for_chihaya(handle, "value", version=version)
+    ConstantArray(dim, value=val)
+}
+
+#######################################################
+#######################################################
+
+#' @export
+#' @import rhdf5
+setMethod("storeDelayedObject", "DelayedAbind", function(x, handle, name, version=package_version("1.1"), ...) {
+    ghandle <- H5Gcreate(handle, name)
+    on.exit(H5Gclose(ghandle), add=TRUE, after=FALSE)
+
+    h5_write_attribute(ghandle, "delayed_type", "operation", scalar=TRUE)
+    h5_write_attribute(ghandle, "delayed_operation", "combine", scalar=TRUE)
+    h5_write_vector(ghandle, "along", x@along - 1L, type="H5T_NATIVE_UINT32", scalar=TRUE)
+
+    shandle <- H5Gcreate(ghandle, "seeds")
+    on.exit(H5Gclose(shandle), add=TRUE, after=FALSE)
+    h5_write_attribute(shandle, "length", length(x@seeds), type="H5T_NATIVE_UINT32", scalar=TRUE)
+
+    for (i in seq_along(x@seeds)) {
+        storeDelayedObject(x@seeds[[i]], shandle, as.character(i - 1L), version=version, ...)
+    }
+
+    invisible(NULL)
+})
+
+#' @import alabaster.base rhdf5 DelayedArray
+chihaya_operation_registry[["combine"]] <- function(handle, version, ...) {
+    shandle <- H5Gopen(handle, "seeds")
+    on.exit(H5Gclose(shandle), add=TRUE, after=FALSE)
+
+    len <- h5_read_attribute(shandle, "length")
+    along <- h5_read_vector(ghandle, "along")
+
+    seeds <- vector("list", len)
+    for (i in seq_len(len)) {
+        x <- local({
+            ihandle <- H5Gopen(shandle, as.character(i - 1L))
+            on.exit(H5Gclose(ihandle))
+            reloadDelayedObject(ihandle, version=version, ...)
+        })
+        if (!is(x, "DelayedArray")) {
+            x <- DelayedArray(x)
+        }
+        seeds[[i]] <- x
+    }
+
+    if (along == 0L) {
+        do.call(arbind, seeds)
+    } else {
+        do.call(acbind, seeds)
+    }
+}
+
+#######################################################
+#######################################################
+
+#' @export
+#' @import rhdf5
+setMethod("storeDelayedObject", "ANY", function(x, handle, name, version=package_version("1.1"), ...) {
+    ghandle <- H5Gcreate(handle, name)
+    on.exit(H5Gclose(ghandle), add=TRUE, after=FALSE)
+
+    h5_write_attribute(ghandle, "delayed_type", "array", scalar=TRUE)
+    h5_write_attribute(ghandle, "delayed_array", "custom alabaster seed array", scalar=TRUE)
+
+    exdir <- file.path(dirname(H5Fget_name(handle)), "seeds")
+    dir.create(exdir, showWarnings=FALSE)
+    n <- length(list.files(exdir))
+    saveObject(x, file.path(exdir, n), ...)
+
+    h5_write_vector(ghandle, "dimensions", dim(x), type="H5T_NATIVE_UINT32", compress=0)
+    h5_write_vector(ghandle, "type", to_value_type(type(x)), scalar=TRUE)
+    h5_write_vector(ghandle, "index", n, type="H5T_NATIVE_UINT32", scalar=TRUE)
+})
+
+#' @import alabaster.base rhdf5 DelayedArray
+chihaya_array_registry[["custom alabaster seed array"]] <- function(handle, version, ...) {
+    index <- h5_read_vector(handle, "index")
+    readObject(file.path(dirname(H5Fget_name(handle)), "seeds", index), ...)
+}
